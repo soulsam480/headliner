f0aeab5694fb81b1a093742e69512e8c3f783f37
soulsam480 <soulsam480@hotmail.com>
2019-11-10 17:20:41 +0530

    service worker update

--- sw.js
+++ sw.js
@@ -1,4 +1,28 @@
 self.addEventListener('fetch', function(event){
 
-  
+    console.log(event.request.url);
+    event.respondWith(
+        caches.match(event.request).then(function(response) {
+          return response || fetch(event.request);
+        })
+      );
+
+});
+
+
+self.addEventListener('install', function(e) {
+ e.waitUntil(
+   caches.open('/').then(function(cache) {
+     return cache.addAll([
+       '/',
+       '/index.html',
+       '/world.html',
+       '/android-chrome-192x192.png',
+       '/apple-touch-icon.png',
+       '/favicon-16x16.png',
+       '/favicon-32x32.png',
+       '/icons-512.png',
+     ]);
+   })
+ );
 });
