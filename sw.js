0c2c78dc6fc32bf075a84ff54fa7d5703395f61e
soulsam480 <soulsam480@hotmail.com>
2019-11-10 18:03:58 +0530

    update

--- sw.js
+++ sw.js
@@ -1,27 +1,34 @@
-self.addEventListener('fetch', function(event){
-
-    console.log(event.request.url);
-    event.respondWith(
-        caches.match(event.request).then(function(response) {
-          return response || fetch(event.request);
-        })
-      );
-
-});
-
+self.addEventListener('fetch', (e) => {
+    e.respondWith(
+      caches.match(e.request).then((r) => {
+            console.log('[Service Worker] Fetching resource: '+e.request.url);
+        return r || fetch(e.request).then((response) => {
+                  return caches.open(cacheName).then((cache) => {
+            console.log('[Service Worker] Caching new resource: '+e.request.url);
+            cache.put(e.request, response.clone());
+            return response;
+          });
+        });
+      })
+    );
+  });
+var cacheName = 'headliner';
 
 self.addEventListener('install', function(e) {
  e.waitUntil(
-   caches.open('').then(function(cache) {
+   caches.open('headliner').then(function(cache) {
      return cache.addAll([
-       '/',
-       '/index.html',
-       '/world.html',
-       '/android-chrome-192x192.png',
-       '/apple-touch-icon.png',
-       '/favicon-16x16.png',
-       '/favicon-32x32.png',
-       '/icons-512.png',
+        './',
+        './index.html',
+        './world.html',
+        './android-chrome-192x192.png',
+       './apple-touch-icon.png',
+       './favicon-16x16.png',
+       './favicon-32x32.png',
+       './icons-512.png',
+       './mstile-150x150.png',
+       './favicon.ico',
+       './safari-pinned-tab.svg'
      ]);
    })
  );
