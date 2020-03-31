17f7d19f335bd0aa48011ac901b090eb7ce7dc3f
soulsam480 <soulsam480@hotmail.com>
2020-03-31 10:23:30 +0530

    main thread split commit

---
+++ assets/js/index.js
@@ -0,0 +1,57 @@
+
+const url =
+  "https://newsapi.org/v2/top-headlines?country=in&pageSize=100&apiKey=05aefe5393fc46d7a6e21a27b16718c0"; // Get 10 random users
+const ul = document.getElementById("news");
+//code for creation of elements for the data display
+function createNode(element) {
+  return document.createElement(element); // Create the type of element you pass in the parameters
+}
+
+function append(parent, el) {
+  return parent.appendChild(el); // Append the second parameter(element) to the first one
+}
+//fetch() to get news from api
+function getData() {
+  fetch(url)
+    .then(resp => resp.json()) // Transform the data into json
+    .then(function(data) {
+      let authors = data.articles;
+      return authors.map(item => {
+        let li = createNode("li"),
+          h4 = createNode("h3");
+        p2 = createNode("p");
+        img = createNode("img");
+        link = createNode("a");
+        line = createNode("span");
+        link4 = createNode("a");
+
+        link4.setAttribute(
+          "onclick",
+          "if (navigator.share) {navigator.share({title:'" +
+            item.title +
+            "',text:'shared from Headliner',url: '" +
+            item.url +
+            "',}).then(() => console.log('Successful share'))}"
+        );
+        link4.innerHTML = `${'<img src="https://img.icons8.com/flat_round/64/000000/share--v1.png"/>'}`;
+        link.setAttribute("href", item.url);
+        link.setAttribute("target", "_blank");
+        img.setAttribute("src", item.urlToImage);
+        h4.innerHTML = `${item.title}`;
+        link.innerHTML = `${"Link to article >>"}`;
+        p2.innerHTML = `${item.description}`;
+        link4.setAttribute("class", "washare");
+        line.innerHTML = `${"<hr>"}`;
+
+        append(li, h4);
+        append(li, img);
+        append(li, p2);
+        append(li, link);
+        append(li, link4);
+        append(li, line);
+        append(ul, li);
+      });
+    });
+}
+
+window.onload = getData();
--- sw.js
+++ sw.js
@@ -1,61 +1,66 @@
-
-
-var cacheName = 'static';
+var cacheName = "static";
 
 // Cache our known resources during install
-self.addEventListener('install', event => {
+self.addEventListener("install", event => {
   event.waitUntil(
-    caches.open(cacheName)
-    .then(cache => cache.addAll([       
-      './assets/style.css',                        
-      './assets/js/jquery-3.4.1.slim.min.js',
-      './assets/js/feednami-client-v1.1.js',
-      './assets/js/lazyload.min.js',
-      './assets/img/android-chrome-192x192.png',
-      './assets/img/apple-touch-icon.png',
-      './assets/img/favicon-16x16.png',
-      './assets/img/favicon-32x32.png',
-      './assets/img/favicon.ico',
-      './assets/img/icons-512.png',
-      './assets/img/mstile-150x150.png',
-      './assets/img/safari-pinned-tab.svg',
-      'offline.html',
-      'privacy-policy.html'
-
-    ]))
+    caches
+      .open(cacheName)
+      .then(cache =>
+        cache.addAll([
+          "./assets/style.css",
+          "./assets/js/index.js",
+          "./assets/js/jquery-3.4.1.slim.min.js",
+          "./assets/js/feednami-client-v1.1.js",
+          "./assets/js/infin.js",
+          "./assets/js/infit.js",
+          "./assets/js/infil.js",
+          "./assets/js/lazyload.min.js",
+          "./assets/img/android-chrome-192x192.png",
+          "./assets/img/apple-touch-icon.png",
+          "./assets/img/favicon-16x16.png",
+          "./assets/img/favicon-32x32.png",
+          "./assets/img/favicon.ico",
+          "./assets/img/icons-512.png",
+          "./assets/img/mstile-150x150.png",
+          "./assets/img/safari-pinned-tab.svg",
+          "offline.html",
+          "privacy-policy.html"
+        ])
+      )
   );
 });
 
+
 // Cache any new resources as they are fetched
-self.addEventListener('fetch', event => {  
-  if(event.request.mode === 'navigate' || (event.request.method === 'GET' && event.request.headers.get('accept').includes('text/html'))){
+self.addEventListener("fetch", event => {
+  if (
+    event.request.mode === "navigate" ||
+    (event.request.method === "GET" &&
+      event.request.headers.get("accept").includes("text/html"))
+  ) {
     event.respondWith(
       fetch(event.request.url).catch(error => {
         // Return the offline page
-        return caches.match('offline.html');
-    })
+        return caches.match("offline.html");
+      })
     );
-  }
-  else{
-    caches.match(event.request, { ignoreSearch: true })         
-    .then(function(response) {
-      if (response) {
-        return response;                                        
-      }
-      var requestToCache = event.request.clone();
+  } else {
+    caches
+      .match(event.request, { ignoreSearch: true })
+      .then(async function (response) {
+        if (response) {
+          return response;
+        }
 
-      return fetch(requestToCache).then(                        
-        function(response) {
-          if(!response || response.status !== 200) {
+        var requestToCache = event.request.clone();
+
+        return fetch(requestToCache).then(function (response) {
+          if (!response || response.status !== 200) {
             return response;
           }
 
           return response;
         });
-    })
+      });
   }
 });
-
-
-
-
